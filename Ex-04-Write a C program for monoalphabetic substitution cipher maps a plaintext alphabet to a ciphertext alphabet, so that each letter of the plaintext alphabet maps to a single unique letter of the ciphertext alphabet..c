69d89022927be9e918ff1eb744e289616801e719
Schifrin-Beckham-192110696 <113084063+Schifrin-Beckham-192110696@users.noreply.github.com>
2023-10-17 12:11:41 +0530

    Update Ex-04-Write a C program for monoalphabetic substitution cipher maps a plaintext alphabet to a ciphertext alphabet, so that each letter of the plaintext alphabet maps to a single unique letter of the ciphertext alphabet..c

--- Ex-04-Write a C program for monoalphabetic substitution cipher maps a plaintext alphabet to a ciphertext alphabet, so that each letter of the plaintext alphabet maps to a single unique letter of the ciphertext alphabet..c
+++ Ex-04-Write a C program for monoalphabetic substitution cipher maps a plaintext alphabet to a ciphertext alphabet, so that each letter of the plaintext alphabet maps to a single unique letter of the ciphertext alphabet..c
@@ -1,35 +1,53 @@
-#include<stdio.h>
-#include<string.h>
-char alpha[27][3] = { { 'a', 'z' }, { 'b', 'y' }, { 'c', 'x' }, { 'd', 'w' }, {
-        'e', 'v' }, { 'f', 'u' }, { 'g', 't' }, { 'h', 's' }, { 'i', 'r' }, {
-        'j', 'q' }, { 'k', 'p' }, { 'l', 'o' }, { 'm', 'n' }, { 'n', 'm' }, {
-        'o', 'l' }, { 'p', 'k' }, { 'q', 'j' }, { 'r', 'i' }, { 's', 'h' }, {
-        't', 'g' }, { 'u', 'f' }, { 'v', 'e' }, { 'w', 'd' }, { 'x', 'c' }, {
-	    'y', 'b' }, { 'z', 'a' } };
-
+#include <stdio.h>
+#include <string.h>
+char alpha[27][3] = {
+    { 'a', 'z' }, { 'b', 'y' }, { 'c', 'x' }, { 'd', 'w' }, { 'e', 'v' },
+    { 'f', 'u' }, { 'g', 't' }, { 'h', 's' }, { 'i', 'r' }, { 'j', 'q' },
+    { 'k', 'p' }, { 'l', 'o' }, { 'm', 'n' }, { 'n', 'm' }, { 'o', 'l' },
+    { 'p', 'k' }, { 'q', 'j' }, { 'r', 'i' }, { 's', 'h' }, { 't', 'g' },
+    { 'u', 'f' }, { 'v', 'e' }, { 'w', 'd' }, { 'x', 'c' }, { 'y', 'b' },
+    { 'z', 'a' }
+};
 char monoalphabetic(char a);
+char monoalphabetic_decrypt(char a);
 
-int main()
-{
-	char str1[10],str[10];
-	printf("Enter the string:");
-	gets(str);
-	for(int i=0;str[i]!='\0';i++)
+int main() {
+    char str[10];
+    printf("Enter the string: ");
+    gets(str);
+    char encrypted[10];
+    char decrypted[10];
+    for (int i = 0; str[i] != '\0'; i++) 
 	{
-		str1[i]=monoalphabetic(str[i]);
-	}
-	printf("After Decryption:%s",str1);
+        encrypted[i] = monoalphabetic(str[i]);
+    }
+    encrypted[strlen(str)] = '\0';
+
+    for (int i = 0; encrypted[i] != '\0'; i++) {
+        decrypted[i] = monoalphabetic_decrypt(encrypted[i]);
+    }
+    decrypted[strlen(encrypted)] = '\0';
+    printf("Cipher Text: %s\n", encrypted);
+    printf("Decrypted Text: %s\n", decrypted);
+    return 0;
 }
 
-char monoalphabetic(char a)
-{
-	int i;
-	for(i=0;i<27;i++)
-	{
-		if(a==alpha[i][0])
-		{
-			break;
-		}
-	}
-	return alpha[i][1];
+char monoalphabetic(char a) {
+    int i;
+    for (i = 0; i < 27; i++) {
+        if (a == alpha[i][0]) {
+            return alpha[i][1];
+        }
+    }
+    return a;
+}
+
+char monoalphabetic_decrypt(char a) {
+    int i;
+    for (i = 0; i < 27; i++) {
+        if (a == alpha[i][1]) {
+            return alpha[i][0];
+        }
+    }
+    return a;
 }
