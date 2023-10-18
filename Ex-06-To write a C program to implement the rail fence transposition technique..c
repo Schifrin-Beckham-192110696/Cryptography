0cba1f3a8219af6aea6db7acd59340771f8b7e6d
Schifrin-Beckham-192110696 <113084063+Schifrin-Beckham-192110696@users.noreply.github.com>
2023-10-18 09:55:44 +0530

    Update Ex-06-To write a C program to implement the rail fence transposition technique..c

--- Ex-06-To write a C program to implement the rail fence transposition technique..c
+++ Ex-06-To write a C program to implement the rail fence transposition technique..c
@@ -1,51 +1,56 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-int main()
-{
- int i,j,len,depth,count,code[100][1000];
- char str[1000];
- printf("Enter the plain text:");
- gets(str);
- len=strlen(str);
- printf("Enter depth:");
- scanf("%d",&depth);
- for(i=0;i<depth;i++)
- {
-  for(j=0;j<len;j++)
-  {
-   code[i][j]=0;
-  }
- }
- count=0;
- j=0;
- while(j<len)
- {
-  if(count%2==0)
-  {
-   for(i=0;i<depth;i++)
-   {
-    code[i][j]=(int)str[j]; 
-    j++;
-   } 
-  }
-  else
-  {
-   for(i=depth-2;i>0;i--)
-   {
-    code[i][j]=(int)str[j];
-    j++;
-   }   
-  } 
-  count++;
- }
- for(i=0;i<depth;i++)
- {
-  for(j=0;j<len;j++)
-  {
-   if(code[i][j]!=0)
-    printf("%c",code[i][j]);
-  }
- }
- printf("\n");
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int rails;
+    char text[1000];
+
+    printf("Enter the depth: ");
+    scanf("%d", &rails);
+
+    printf("Enter the text: ");
+    scanf(" %s", text);
+
+    int length = strlen(text);
+    char code[rails][length];
+
+    int count = 0;
+    int len = 0;
+    int j = 0;
+    int direction = 1;
+
+    for (int i = 0; i < rails; i++) {
+        for (int j = 0; j < length; j++) {
+            code[i][j] = ' ';
+        }
+    }
+
+    while (j < length) {
+        code[count][j] = text[j];
+        j++;
+
+        if (count == 0) {
+            direction = 1;
+        } else if (count == rails - 1) {
+            direction = -1;
+        }
+
+        count += direction;
+    }
+    printf("Rail Fence Pattern:\n");
+    for (int i = 0; i < rails; i++) {
+        for (int j = 0; j < length; j++) {
+            printf("%c ", code[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Encrypted Text: ");
+    for (int i = 0; i < rails; i++) {
+        for (int j = 0; j < length; j++) {
+            if (code[i][j] != ' ') {
+                printf("%c", code[i][j]);
+            }
+        }
+    }
+    printf("\n");
 }
