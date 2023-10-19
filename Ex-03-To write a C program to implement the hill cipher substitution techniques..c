f87310112863bffdbc0e80f09012c2858d53e5d9
Schifrin-Beckham-192110696 <113084063+Schifrin-Beckham-192110696@users.noreply.github.com>
2023-10-19 08:32:34 +0530

    Update Ex-03-To write a C program to implement the hill cipher substitution techniques..c

--- Ex-03-To write a C program to implement the hill cipher substitution techniques..c
+++ Ex-03-To write a C program to implement the hill cipher substitution techniques..c
@@ -1,38 +1,114 @@
-#include<stdio.h>
-#include<string.h>
-int main() 
-{
-    unsigned int a[3][3] = { { 6, 24, 1 }, { 13, 16, 10 }, { 20, 17, 15 } };
-    unsigned int b[3][3] = { { 8, 5, 10 }, { 21, 8, 21 }, { 21, 12, 8 } };
-    int i, j;
-    unsigned int c[20], d[20];
-    char msg[20];
-    int determinant = 0, t = 0;
-    printf("Enter plain text\n ");
-    scanf("%s", msg);
-    for (i = 0; i < 3; i++) {
-        c[i] = msg[i] - 65;
-        printf("%d ", c[i]);
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define MOD 26
+
+// Function to calculate the greatest common divisor (GCD) of two numbers
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+// Function to calculate the modular multiplicative inverse using the Extended Euclidean Algorithm
+int modInverse(int a, int m) {
+    a = a % m;
+    for (int x = 1; x < m; x++) {
+        if ((a * x) % m == 1)
+            return x;
     }
-    for (i = 0; i < 3; i++) {
-        t = 0;
-        for (j = 0; j < 3; j++) {
-            t = t + (a[i][j] * c[j]);
+    return -1; // Inverse doesn't exist
+}
+
+// Function to perform Hill cipher encryption
+void hillEncrypt(int key[][2], char *text, int len) {
+    int result[len];
+    int size = 2;
+
+    for (int i = 0; i < len; i += size) {
+        for (int j = 0; j < size; j++) {
+            result[i + j] = 0;
+            for (int k = 0; k < size; k++) {
+                result[i + j] += key[j][k] * (text[i + k] - 'a');
+            }
+            result[i + j] %= 26;
+            text[i + j] = 'a' + result[i + j];
         }
-        d[i] = t % 26;
     }
-    printf("\nEncrypted Cipher Text :");
-    for (i = 0; i < 3; i++)
-        printf(" %c", d[i] + 65);
-    for (i = 0; i < 3; i++) {
-    	  t = 0;
-        for (j = 0; j < 3; j++) {
-            t = t + (b[i][j] * d[j]);
+}
+
+// Function to perform Hill cipher decryption
+void hillDecrypt(int key[][2], char *text, int len) {
+    int result[len];
+    int size = 2;
+    int determinant = key[0][0] * key[1][1] - key[0][1] * key[1][0];
+
+    if (determinant == 0 || gcd(determinant, MOD) != 1) {
+        printf("Matrix not invertible.\n");
+        return;
+    }
+
+    int inverse = modInverse(determinant, MOD);
+    if (inverse == -1) {
+        printf("Matrix not invertible.\n");
+        return;
+    }
+
+    int a = (key[1][1] * inverse) % MOD;
+    int b = (MOD - key[0][1]) * inverse % MOD;
+    int c = (MOD - key[1][0]) * inverse % MOD;
+    int d = (key[0][0] * inverse) % MOD;
+
+    for (int i = 0; i < len; i += size) {
+        for (int j = 0; j < size; j++) {
+            result[i + j] = 0;
+            for (int k = 0; k < size; k++) {
+                result[i + j] += (a * (text[i + k] - 'a') + b * (text[i + k + 1] - 'a')) % MOD;
+                result[i + j] += (c * (text[i + k] - 'a') + d * (text[i + k + 1] - 'a')) % MOD;
+                result[i + j] %= MOD;
+                text[i + j] = 'a' + result[i + j];
+            }
         }
-        c[i] = t % 26;
     }
-    printf("\nDecrypted Cipher Text :");
-    for (i = 0; i < 3; i++)
-        printf(" %c", c[i] + 65);
+}
+
+int main() {
+    char text[100];
+    int key[2][2];
+    int size = 2;
+    int len, choice;
+
+    printf("Choose an option:\n");
+    printf("1. Encrypt\n");
+    printf("2. Decrypt\n");
+    printf("Enter your choice (1 or 2): ");
+    scanf("%d", &choice);
+
+    if (choice != 1 && choice != 2) {
+        printf("Invalid choice. Please choose 1 for encryption or 2 for decryption.\n");
+        return 1;
+    }
+
+    printf("Enter the key matrix (%dx%d):\n", size, size);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            scanf("%d", &key[i][j]);
+        }
+    }
+
+    printf("Enter the text (in lowercase, no spaces): ");
+    scanf("%s", text);
+    len = strlen(text);
+
+    if (choice == 1) {
+        hillEncrypt(key, text, len);
+        printf("Encrypted text: %s\n", text);
+    } else {
+        hillDecrypt(key, text, len);
+        printf("Decrypted text: %s\n", text);
+    }
+
     return 0;
 }
